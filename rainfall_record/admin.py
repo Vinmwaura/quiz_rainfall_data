1d4a12066dfd636085d160aaf98248c9b7438090
Vincent <vinmwega@gmail.com>
2016-04-21 01:51:45 +0300

    Added validation to Rainfall model to prevent negative numbers and future dates

--- rainfall_record/admin.py
+++ rainfall_record/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Region, Rainfall
 # Register your models here.
+
+
+class RainfallAdmin(admin.ModelAdmin):
+    model = Rainfall
+    list_filter = ['date', 'region']
+    search_fields = ['date']
+
+admin.site.register(Region)
+admin.site.register(Rainfall, RainfallAdmin)
--- rainfall_record/models.py
+++ rainfall_record/models.py
@@ -1,19 +1,38 @@
 from django.db import models
-
+from django.utils.encoding import python_2_unicode_compatible
+from django.core.validators import MinValueValidator
 # Create your models here.
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from datetime import date
 
 
+def validate_date(value):
+    date_today = date.today()
+    if value > date_today:
+        raise ValidationError(
+            _('%(value)s is invalid. You can not set date in the future.'),
+            params={'value': value},
+        )
+
+
+@python_2_unicode_compatible
 class Region(models.Model):
     region_name = models.CharField(max_length=128, unique=True)
 
     def __str__(self):
-        return self.region_name
+        return str(self.region_name)
 
 
+@python_2_unicode_compatible
 class Rainfall(models.Model):
     region = models.ForeignKey(Region)
-    amount = models.DecimalField(max_digits=5, decimal_places=1, default=0.00)
-    date = models.DateTimeField()
+    amount = models.DecimalField(
+        max_digits=5,
+        decimal_places=1,
+        default=0.0,
+        validators=[MinValueValidator(0.0)])
+    date = models.DateField(validators=[validate_date])
 
     def __str__(self):
-        return "Region: ", self.region, " Date: ", self.date, " Amount: ", self.amount
+        return str(self.region) + " " + str(self.date)
