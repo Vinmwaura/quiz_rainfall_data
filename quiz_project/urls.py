55237b9e1ed97219a86d1fea0244b35ba4bd9ee0
Vincent <vinmwega@gmail.com>
2016-04-22 02:17:41 +0300

    Improved template and forms module to be user-friendly

--- quiz_project/urls.py
+++ quiz_project/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^rainfall/', include('rainfall_record.urls', namespace = "rainfall")),
+    url(r'^rainfall/', include('rainfall_record.urls', namespace="rainfall")),
 ]
--- rainfall_record/forms.py
+++ rainfall_record/forms.py
@@ -6,17 +6,24 @@ import datetime
 
 class RainfallForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
+        # Class inheritance is to allow choices of region field to
+        # be obtained from queryset of Region model
         super(RainfallForm, self).__init__(*args, **kwargs)
+        # region field that uses data from Region model
         region = forms.ChoiceField(
             choices=Region.objects.all(),
             widget=forms.Select,
             required=True)
 
+    # Amount field that gets decimal values of rainfall
+    # can't benegative value
     amount = forms.DecimalField(
         max_digits=5,
         decimal_places=1,
         validators=[MinValueValidator(0.0)],
         required=True)
+
+    # Date the record was taken, can't be greater than the current date
     date = forms.DateField(
         widget=forms.SelectDateWidget())
 
@@ -26,10 +33,13 @@ class RainfallForm(forms.ModelForm):
 
 
 class RecordForm(forms.Form):
-    min_year = 1990
-    current_year = datetime.date.today().year
+    min_year = 2000  # Sets the lowest year that can be selected
+    current_year = datetime.date.today().year  # Gets the current year
+    # Validates if current year selected is greater than minimun set
     if min_year < current_year:
-        years = [(x, x) for x in range(min_year, current_year + 1)]
+        # Sets the choices starting with the current year
+        # and decreases to the minimum
+        years = [(x, x) for x in range(current_year, min_year, -1)]
     else:
         years = []
 
